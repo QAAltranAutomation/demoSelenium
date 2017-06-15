c8241d3272a068702dd9e3bbe0cf2794c2d23a28
ALTRAN_ES\sonia.cepedano <sonia.cepedanoduenas@altran.com>
2017-06-15 10:39:50 +0200

    Dir de login

--- src/test/java/tests/projecBase/Config/Config.java
+++ src/test/java/tests/projecBase/Config/Config.java
@@ -5,7 +5,7 @@ public class Config {
   public static String defaultBrowserVersion = System.getProperty("version", "59");
   public static String defaultPlatform = System.getProperty("platform", "local");
 
-  public static String baseUrl = System.getProperty("baseUrl", "http://www.gmail.com");
+  public static String baseUrl = System.getProperty("baseUrl", "https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 
 
 }
