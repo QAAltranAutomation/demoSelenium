9f47752e91b8cfcf73272cb3fe773acea71b8932
ALTRAN_ES\sonia.cepedano <sonia.cepedanoduenas@altran.com>
2017-06-16 08:41:42 +0200

    Navegador

--- src/test/java/tests/projecBase/Config/Config.java
+++ src/test/java/tests/projecBase/Config/Config.java
@@ -1,8 +1,8 @@
 package tests.projecBase.Config;
 
 public class Config {
-  public static String defaultBrowser = System.getProperty("browser", "firefox");
-  public static String defaultBrowserVersion = System.getProperty("version", "53");
+  public static String defaultBrowser = System.getProperty("browser", "chrome");
+  public static String defaultBrowserVersion = System.getProperty("version", "59");
   public static String defaultPlatform = System.getProperty("platform", "local");
 
   public static String baseUrl = System.getProperty("https://www.google.com/gmail/about/#");
